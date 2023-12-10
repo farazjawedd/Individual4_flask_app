53f748d702352eb4e4beb9998b4ecae3c360e5e5
Faraz Jawed <farazjawed@Farazs-Air.lan>
2023-12-10 15:22:32 -0500

    pushed

--- summarizer_app/app.py
+++ summarizer_app/app.py
@@ -1,4 +1,4 @@
-'''Imports'''
+"""Imports"""
 from flask import Flask, request, render_template, redirect, url_for
 import app_logic
 
@@ -6,27 +6,30 @@ app = Flask(__name__)
 
 summary = None
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     global summary
-    if request.method == 'POST':
-        input_text = request.form.get('input')
-        max_length = int(request.form.get('max_length', 130))
-        min_length = int(request.form.get('min_length', 30))
+    if request.method == "POST":
+        input_text = request.form.get("input")
+        max_length = int(request.form.get("max_length", 130))
+        min_length = int(request.form.get("min_length", 30))
 
         if input_text:
-            response = app_logic.query({
-                "inputs": input_text
-            }, max_length=max_length, min_length=min_length)
+            response = app_logic.query(
+                {"inputs": input_text}, max_length=max_length, min_length=min_length
+            )
             summary = response[0]  # assuming the response is a list
-        return redirect(url_for('summary_view'))
+        return redirect(url_for("summary_view"))
 
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/summary', methods=['GET'])
+
+@app.route("/summary", methods=["GET"])
 def summary_view():
     global summary
-    return render_template('summary.html', summary=summary)
+    return render_template("summary.html", summary=summary)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
