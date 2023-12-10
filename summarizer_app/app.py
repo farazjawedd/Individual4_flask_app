60af5365603c4a11b907beed098d03e2b2e22271
farazjawedd <101464414+farazjawedd@users.noreply.github.com>
2023-12-10 18:21:25 +0000

    add done

--- summarizer_app/app.py
+++ summarizer_app/app.py
@@ -1,8 +1,31 @@
-from claude import Claude
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+import app_logic
 
-claude = Claude()
+app = Flask(__name__)
 
-text = "Your text to summarize"
+summary = None
 
-summary = claude.summarize(text)
-print(summary)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global summary
+    if request.method == 'POST':
+        input_text = request.form.get('input')
+        max_length = int(request.form.get('max_length', 130))
+        min_length = int(request.form.get('min_length', 30))
+
+        if input_text:
+            response = app_logic.query({
+                "inputs": input_text
+            }, max_length=max_length, min_length=min_length)
+            summary = response[0]  # assuming the response is a list
+        return redirect(url_for('summary_view'))
+
+    return render_template('index.html')
+
+@app.route('/summary', methods=['GET'])
+def summary_view():
+    global summary
+    return render_template('summary.html', summary=summary)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- summarizer_app/app_logic.py
+++ summarizer_app/app_logic.py
@@ -1,35 +1,66 @@
-from transformers import pipeline
+import requests
 
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn", min_length=60)
+API_TOKEN = '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'
 
-def summarize(text, length=None):
-    """
-    Summarize input text
-    """
-    if length:
-        # Truncate summary if length provided
-        summary = summarizer(text, max_length=length, min_length=60)[0]['summary_text']   
-    else:
-        # Default summarization
-        summary = summarizer(text)[0]['summary_text']
+API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-    return summary
-
-
-def analyze_summary(original, summary):
-    """
-    Analyze the similarity between original text 
-    and generated summary
-    """
-    import difflib
-    diff = difflib.SequenceMatcher(None, original, summary)
-
-    # Calculate % similarity
-    match_pct = diff.ratio() * 100
-
-    results = {
-        "word_count": len(summary.split()),        
-        "similarity": round(match_pct, 1)
+def query(payload, max_length=130, min_length=30):
+    payload["parameters"] = {
+        "max_length": max_length,
+        "min_length": min_length
     }
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+	
+input = "The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct."
 
-    return results
+max_length = 20
+min_length = 10
+
+output = query({
+    "inputs": input
+}, max_length=max_length, min_length=min_length)
+
+print (output)
+
+
+
+
+
+
+
+
+# summarizer = pipeline("summarization", model="facebook/bart-large-cnn", min_length=60)
+
+# def summarize(text, length=None):
+#     """
+#     Summarize input text
+#     """
+#     if length:
+#         # Truncate summary if length provided
+#         summary = summarizer(text, max_length=length, min_length=60)[0]['summary_text']   
+#     else:
+#         # Default summarization
+#         summary = summarizer(text)[0]['summary_text']
+
+#     return summary
+
+
+# def analyze_summary(original, summary):
+#     """
+#     Analyze the similarity between original text 
+#     and generated summary
+#     """
+#     import difflib
+#     diff = difflib.SequenceMatcher(None, original, summary)
+
+#     # Calculate % similarity
+#     match_pct = diff.ratio() * 100
+
+#     results = {
+#         "word_count": len(summary.split()),        
+#         "similarity": round(match_pct, 1)
+#     }
+
+#     return results
